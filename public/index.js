8f17c7348d2dc9245ea1d0b4c15086cdfc83c2d1
Silloty1 <nguyen.son.ha.2404@gmail.com>
2018-01-20 22:07:49 +0100

    Commit 4 et 5

--- public/index.js
+++ public/index.js
@@ -161,52 +161,6 @@ for (const item of deliveries)
 }
 */
 
-//Exercice 4
-for (const item of deliveries)
-{
-    let priceKm = 0;
-    let priceVl =0;
-    for (const item2 of truckers){
-      if (item.truckerId == item2.id)
-      {
-        priceKm = item2.pricePerKm;
-        priceVl = item2.pricePerVolume;
-        break;
-      }
-    }
-    if (item.volume >= 5)
-    {
-      priceVl = priceVl * 0.9;
-    }
-    else if (item.volume >= 10) {
-      priceVl = priceVl * 0.7;
-    }
-    else if (item.volume >= 25) {
-       priceVl = priceVl * 0.5;
-    }
-
-
-
-    const shipping_price = (item.distance *priceKm) + (item.volume*priceVl);
-    shipping_price;
-    console.log("Shipping Price : " + shipping_price);
-    let priceTax = shipping_price*0.3;
-    let inssurance = priceTax*0.5;
-    console.log("Insurance : "+ inssurance);
-    let treasury = Math.round(item.distance/500);
-    console.log("Treasury : " + treasury);
-    let rest = priceTax - inssurance - treasury;
-    console.log("Rest : " + rest);
-    let convargo = 0;
-    if (item.options.deductibleReduction == true)
-    {
-       let deductible_charge = item.volume;
-       console.log("Deductible Charge : " + deductible_charge);
-       convargo =  deductible_charge;
-    }
-    convargo += rest;
-    console.log("Total Convargo : " + convargo);
-}
 
 
 
@@ -214,7 +168,7 @@ for (const item of deliveries)
 
 //list of actors for payment
 //useful from step 5
-const actors = [{
+let actors = [{
   'deliveryId': 'bba9500c-fd9e-453f-abf1-4cd8f52af377',
   'payment': [{
     'who': 'shipper',
@@ -284,6 +238,90 @@ const actors = [{
     'amount': 0
   }]
 }];
+
+
+
+//Exercice 4+5
+for (const item of deliveries)
+{
+    let priceKm = 0;
+    let priceVl =0;
+    for (const item2 of truckers){
+      if (item.truckerId == item2.id)
+      {
+        priceKm = item2.pricePerKm;
+        priceVl = item2.pricePerVolume;
+        break;
+      }
+    }
+    if (item.volume >= 5)
+    {
+      priceVl = priceVl * 0.9;
+    }
+    else if (item.volume >= 10) {
+      priceVl = priceVl * 0.7;
+    }
+    else if (item.volume >= 25) {
+       priceVl = priceVl * 0.5;
+    }
+
+
+
+    let shipping_price = (item.distance *priceKm) + (item.volume*priceVl);
+    console.log("Shipping Price : " + shipping_price);
+    let priceTax = shipping_price*0.3;
+    let inssurance = priceTax*0.5;
+    console.log("Insurance : "+ inssurance);
+    let treasury = Math.round(item.distance/500);
+    console.log("Treasury : " + treasury);
+    let rest = priceTax - inssurance - treasury;
+    console.log("Rest : " + rest);
+    let convargo = 0;
+    let deductible_charge;
+    if (item.options.deductibleReduction == true)
+    {
+       deductible_charge = item.volume + 200;
+       console.log("Deductible Charge : " + deductible_charge);
+       convargo =  deductible_charge + rest;
+    }
+    else
+    {
+      convargo = convargo + rest + 1000;
+      deductible_charge = 1000;
+    }
+    console.log("Total Convargo : " + convargo);
+
+    var payment = [
+      {
+          "who": "shipper",
+          "type": "debit",
+          "amount": shipping_price + deductible_charge
+        },
+        {
+          "who": "trucker",
+          "type": "credit",
+          "amount": shipping_price*0.7
+        },
+        {
+          "who": "insurance",
+          "type": "credit",
+          "amount": inssurance
+        },
+        {
+          "who": "treasury",
+          "type": "credit",
+          "amount": treasury
+        },
+        {
+          "who": "convargo",
+          "type": "credit",
+          "amount": convargo
+        }
+
+    ]
+    console.log(payment);
+}
+
 
 console.log(truckers);
 console.log(deliveries);
